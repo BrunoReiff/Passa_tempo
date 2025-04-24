d5c415a33c57b7ba644129cf69aa8310c0cb0d28
Bruno <81494923+pitholicho@users.noreply.github.com>
2025-04-23 23:09:56 -0300

    Add files via upload

--- Jogodabomba.c
+++ Jogodabomba.c
@@ -20,6 +20,9 @@ int MenuCampo()
     char input[100];
 
     printf("-------- Bem vindo ao Jogo da Bomba --------\n");
+    printf("\tO jogo da bomba consiste em escolher posicoes do campo minado.\n");
+    printf("\tSe escolher um lugar sem bomba, recebe ponto.\n");
+    printf("\tSe escolher um lugar com bomba, perde.\n");
     printf("Escolha o tamanho do campo minado\n");
     printf("1 -> 4 x 4\n");
     printf("2 -> 5 x 5\n");
@@ -78,11 +81,11 @@ int DecideQntBombas(int campo , int dif)
     return qnt;
 }
 
-int MenuDificuldade(int campo)
+int MenuDificuldade()
 {
     int slc_dif, verif = 0;
     char input[100];
-    printf("Escolha a dificuldade do campo minado\n");
+    printf("\nEscolha a dificuldade do campo minado\n");
     printf("1 -> Facil\n");
     printf("2 -> Medio\n");
     printf("3 -> Dificil\n");
@@ -110,18 +113,18 @@ int MenuDificuldade(int campo)
     switch(slc_dif){
         case 1:
             printf("Voce escolheu a dificuldade Facil");
-            return DecideQntBombas(campo, 1);
+            return 1;
         case 2:
             printf("Voce escolheu a dificuldade Medio");
-            return DecideQntBombas(campo, 2);
+            return 2;
         case 3:
             printf("Voce escolheu a dificuldade Dificil");
-            return DecideQntBombas(campo, 3);
+            return 3;
         case 4:
             printf("Voce escolheu a dificuldade Ultra Dificil");
-            return DecideQntBombas(campo, 4);
+            return 4;
         default:
-            return DecideQntBombas(campo, 2);
+            return 2;
     }
 }
 
@@ -174,31 +177,112 @@ char **CriaCampo(int l)
     return campo;
 }
 
-void jogar(char **campo)
-{
-    
 
+int jogar(char **campo, char **camposembomba, int dif, int l)
+{
+    int verif1 = 0, c = 0, coluna, linha;
+    char input_coluna[100], input_linha [100];
+    printf("\n\n");
+    do{
+        printf("\n-------- Rodada numero %i --------\n", c + 1);
+        for(int i = 0; i < l; i++){
+            printf("%i  ", i);
+            for(int j = 0; j < l; j++){
+                printf("%c ", camposembomba[i][j]);
+            }
+            printf("\n");
+        }
+
+        do{
+            printf("Digite uma linha(0-%i): ", l-1);
+            if(fgets(input_linha, sizeof(input_linha), stdin) == NULL){
+                continue;
+            }
+            input_linha[strcspn(input_linha, "\n")] = '\0';
+    
+            if(inteiro(input_linha)){
+                 linha = atoi(input_linha);
+    
+                if(linha >= 0 && linha < l){
+                    verif1 = 1;
+                } else {
+                    printf("ERRO!! Digite um numero entre 0 e %i.\n", l-1);
+                }
+            } else {
+                printf("ERRO!! Digite apenas um numero inteiro.\n");
+            }
+        }while(verif1 == 0);
+       
+        verif1 = 0;
+        do{
+            printf("Digite uma coluna(0-%i): ", l-1);
+            if(fgets(input_coluna, sizeof(input_coluna), stdin) == NULL){
+                continue;
+            }
+            input_coluna[strcspn(input_coluna, "\n")] = '\0';
+    
+            if(inteiro(input_coluna)){
+                 coluna = atoi(input_coluna);
+    
+                if(coluna >= 0 && coluna < l){
+                    verif1 = 1;
+                } else {
+                    printf("ERRO!! Digite um numero entre 0 e %i.\n", l-1);
+                }
+            } else {
+                printf("ERRO!! Digite apenas um numero inteiro.\n");
+            }
+        }while(verif1 == 0);
+        
+        if(campo[linha][coluna] == '+'){
+            printf("\n\tBOMBAAAAAA!!!!\n");
+            return c;
+        } else {
+            if(camposembomba[linha][coluna] == '*'){
+                printf("Voce jah jogou nessa posicao. Tente outra!\n");
+            }else {
+                printf("\tOTIMA JOGADA!!!\n");
+                camposembomba[linha][coluna] = '*';
+                c++;
+            }
+        }
+    }while(c < dif);
+    int pontuacao = c * dif;
+    return pontuacao;
+}
+
+char **CopiaCampo(char **campo, int l)
+{
+    char **campo_novo = (char **) malloc(l * sizeof(char*));
+    for(int i = 0; i < l; i++){
+        campo_novo[i] = (char *) malloc(l * sizeof(char));
+        for(int j = 0; j < l; j++){
+            campo_novo[i][j] = campo[i][j];
+        } 
+    }
+    return campo_novo;
 }
 
 
 int main()
 {
-    int slc_campo, slc_dif;
-    char **campo;
+    int slc_campo, slc_dif, qntbomba;
+    char **campo, **camposembomba;
     slc_campo = MenuCampo();
     slc_dif = MenuDificuldade(slc_campo);
+    printf("%i", slc_dif);
+    qntbomba = DecideQntBombas(slc_campo, slc_dif);
     campo = CriaCampo(slc_campo);
     campo = PreencheCampo(campo, slc_campo);
-    campo = SorteiaBomba(campo, slc_campo, slc_dif);
-    
-    printf("\n");
-    for(int i = 0; i < slc_campo; i++){
-        for(int j = 0; j < slc_campo; j++){
-            printf("%c ", campo[i][j]);
-        }
-        printf("\n");
-    }
+    camposembomba = CopiaCampo(campo, slc_campo);
 
+    campo = SorteiaBomba(campo, slc_campo, qntbomba);
+    
+    int pontuacao;
+    pontuacao = jogar(campo, camposembomba, slc_dif, slc_campo);
+
+    printf("-------- Fim de jogo! --------\n");
+    printf("Sua pontuacao final foi %i", pontuacao);
     
     return 0;
 }
